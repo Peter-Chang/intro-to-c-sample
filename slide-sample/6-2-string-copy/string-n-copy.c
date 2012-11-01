c708c4aebb626ca97899ac5aa5a8ba315dfb0184
Peter Chang <phchang@sslab.cs.nthu.edu.tw>
2012-11-01 09:23:09 +0800

    modify strncpy

--- slide-sample/6-2-string-copy/string-n-copy.c
+++ slide-sample/6-2-string-copy/string-n-copy.c
@@ -6,13 +6,13 @@ int main(int argc, const char *argv[])
 	char first_string[] = "Hello world!";
 	char second_string[13];
 	
-	printf("Before strcpy()\n");
+	printf("Before strncpy()\n");
 	printf( "First string: %s\n", first_string );
 	printf( "Second string: %s\n", second_string );
 
 	strncpy(second_string, first_string, 5);
 
-	printf("After strcpy()\n");
+	printf("After strncpy()\n");
 	printf( "First string: %s\n", first_string );
 	printf( "Second string: %s\n", second_string );
 
